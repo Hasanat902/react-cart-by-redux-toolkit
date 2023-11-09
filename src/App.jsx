45a1e59805b2bf2adb0a36c7ddf290ca6d1df0e4
Hasanat902 <abulhasanat902@gmail.com>
2023-11-09 12:09:16 +0600

    project initialized

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useEffect } from 'react';
+import './App.css'
+import Product from './components/Product';
+
+function App() {
+
+  useEffect( () => {
+    
+  }, [])
+
+  return (
+    <>
+      <Product></Product>
+    </>
+  )
+}
+
+export default App;
